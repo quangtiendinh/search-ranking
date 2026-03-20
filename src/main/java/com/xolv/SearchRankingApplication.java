e112c3a80f603f0adb4ca87cd0a7d32fb7e1e595
quangtiendinh <dinhquangtien0169@gmail.com>
2026-03-20 22:16:52 +0700

    ticket-id: Init project

---
+++ src/main/java/com/xolv/SearchRankingApplication.java
@@ -0,0 +1,13 @@
+package com.xolv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchRankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchRankingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xolv/SearchRankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xolv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchRankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
